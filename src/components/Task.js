d052c92eb03002391e6192360189d0581209e7d5
saurabs@patriot1tech.com <saurav540@yahoo.com>
2022-09-28 15:43:24 -0700

    task update fixed

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,29 +1,40 @@
-import { FaTimes, FaCheck } from 'react-icons/fa'
-import React from 'react'
-
-
+import { FaTimes, FaCheck } from "react-icons/fa";
+import React from "react";
 
 const Task = ({ task, onDelete, onToggle, onComplete, onDecomplete }) => {
   return (
-    <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
-      <h3>{task.text} 
-      <FaTimes style={{color: 'red', cursor: 'pointer'}} onClick={() => onDelete(task.id)}/>
+    <div
+      className={`task ${task.reminder ? "reminder" : ""}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
+      <h3>
+        {task.text}
+        <FaTimes
+          style={{ color: "red", cursor: "pointer" }}
+          onClick={() => onDelete(task.id)}
+        />
       </h3>
       <p>{task.day}</p>
-      <button onClick={() => {
-        if (task.status === "complete") {
-          onDecomplete(task.id)
-        } else {
-          onComplete(task.id)
-        }
-      }}>
+      <button
+        onClick={() => {
+          if (task.status === "complete") {
+            onDecomplete(task.id);
+          } else {
+            onComplete(task.id);
+          }
+        }}
+      >
         Mark {task.status === "complete" ? "Incomplete" : "Complete"}
       </button>
       <div>
-      {task.status === "complete" ? <FaCheck style={{color: "green"}}></FaCheck> : ''}
+        {task.status === "complete" ? (
+          <FaCheck style={{ color: "green" }}></FaCheck>
+        ) : (
+          ""
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,23 +1,29 @@
-import Task from './Task'
-import React from 'react'
-import { useLocation } from 'react-router-dom'
-
-
+import Task from "./Task";
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Tasks = ({ tasks, onDelete, onToggle, onComplete, onDecomplete }) => {
   const location = useLocation();
   return (
     <>
       {tasks.map((task) => {
-          let completedTasksCheckValue = location.pathname === "/completed" ? "complete" : "incomplete";
-
-          if (task.status === completedTasksCheckValue) {
-            return <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} onComplete={onComplete} onDecomplete={onDecomplete}/>
-          }
-      }
-      )}
+        let completedTasksCheckValue =
+          location.pathname === "/completed" ? "complete" : "incomplete";
+        if (task.status === completedTasksCheckValue) {
+          return (
+            <Task
+              key={task.id}
+              task={task}
+              onDelete={onDelete}
+              onToggle={onToggle}
+              onComplete={onComplete}
+              onDecomplete={onDecomplete}
+            />
+          );
+        }
+      })}
     </>
-  )
-}
+  );
+};
 
-export default Tasks
+export default Tasks;
